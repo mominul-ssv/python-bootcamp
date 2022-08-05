fe26d9e95f2861e3155301e8a06abd5ce6e28793
Mominul Islam <70751323+Levarenox@users.noreply.github.com>
2022-08-05 13:35:27 +0600

    regular increment

--- 2_Primitive_Types/app.py
+++ 2_Primitive_Types/app.py
@@ -1,6 +1,8 @@
 import math
 
-print("# ============================================= Variables =========================================== #")
+print(
+    "# ============================================= Variables =========================================== #"
+)
 
 # primitive types:
 # 1. strings
@@ -14,7 +16,9 @@ print(students_count)
 print(rating)
 print(is_published)
 
-print("# ============================================= Strings =========================================== #")
+print(
+    "# ============================================= Strings =========================================== #"
+)
 
 course = "Python Programming"
 print(course)
@@ -29,14 +33,17 @@ print("course[-1] = " + course[-1])  # end character of the string
 print("course[0:3] = " + course[0:3])
 print("course[0:] = " + course[0:])
 print("course[:3] = " + course[:3])
+print("course[3:] = " + course[3:])
 print("course[:] = " + course[:])
 
-print("# ============================================= Escape Sequences =========================================== #")
+print(
+    "# ============================================= Escape Sequences =========================================== #"
+)
 
-course = "Python \"Programming\" "
+course = 'Python "Programming" '
 print(course)
 
-course = "Python \'Programming\' "
+course = "Python 'Programming' "
 print(course)
 
 course = "Python \\Programming\\ "
@@ -45,7 +52,9 @@ print(course)
 course = "Python \nProgramming "
 print(course)
 
-print("# ============================================= Formatted Strings =========================================== #")
+print(
+    "# ============================================= Formatted Strings =========================================== #"
+)
 
 first = "Mominul"
 last = "Islam"
@@ -61,7 +70,9 @@ print(full)
 full = f"{first} {12 + 2}"
 print(full)
 
-print("# ============================================= String Methods =========================================== #")
+print(
+    "# ============================================= String Methods =========================================== #"
+)
 
 course = " Python programming "
 
@@ -77,7 +88,9 @@ print("Pro" in course)
 print("pro" in course)
 print("swift" not in course)
 
-print("# ============================================= Numbers =========================================== #")
+print(
+    "# ============================================= Numbers =========================================== #"
+)
 
 x = 1
 x = 1.1
@@ -86,16 +99,18 @@ x = 1 + 2j  # complex number a + bi
 print(10 + 3)
 print(10 - 3)
 print(10 * 3)
-print(10 / 3)   # for float
+print(10 / 3)  # for float
 print(10 // 3)  # for int
-print(10 % 3)   # modulus => remainder of a division
+print(10 % 3)  # modulus => remainder of a division
 print(10 ** 3)  # exponent => 10 to the power of 3
 
 x = 10
 x = x + 3
 x += 3  # augmented addition operator
 
-print("# ============================================= Working with Numbers =========================================== #")
+print(
+    "# ============================================= Working with Numbers =========================================== #"
+)
 
 print(round(2.9))  # round the number
 print(abs(-2.9))  # absolute value of a number
